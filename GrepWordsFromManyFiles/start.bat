af0f8e92e7c130ccb2959d8798940492f64bbff2
ta-watanabe <ta-watanabe@link-tech.jp>
2023-08-31 17:41:14 +0900

    文字数制限を設定,ヘッダー8000文字制限 ヘッダーを考慮すれば,8000文字まで, 考慮しなければ,4100項目程度まで検索可能

--- GrepWordsFromManyFiles/start.bat
+++ GrepWordsFromManyFiles/start.bat
@@ -1,6 +1,27 @@
 @echo off
 setlocal enabledelayedexpansion
 
+:: Set the character count limit
+set charLimit=8000
+
+:: Check command-line argument
+if "%~1"=="-y" (
+    set check=Y
+) else if "%~1"=="-n" (
+    set check=N
+) else (
+    :: Ask the user if no command-line argument is provided
+    goto startCheckCaracterCount
+)
+:endCheckCharacterCount
+
+:: function A Do or Not Check Caracter Count
+goto nextprocessA
+:startCheckCaracterCount
+set /p check=Do you want to check the character count? (Y/N): 
+goto endCheckCharacterCount
+:nextprocessA
+
 :: FOR DEBUG  set the start time
 set "start_time=%time%"
 
@@ -12,6 +33,41 @@ for /f %%a in (grep_list.txt) do (
     set "header=!header!,%%a"
 )
 
+:: Proceed if 'Y' or 'y' is selected
+if /i "%check%"=="Y" (
+    :: Check if header character count exceeds the limit
+    set count=0
+    :loopHeader
+    if defined header (
+        set "char=!header:~0,1!"
+        set "header=!header:~1!"
+        set /a count+=1
+        goto loopHeader
+    )
+    if !count! geq %charLimit% (
+        goto startContinueNextProcess
+    ) else (
+        echo characters : !count! under the char Limit : %charLimit% 
+    )
+)
+:endContinueNextProcess
+
+:: Function B Continue or Not Next Process
+goto nextprocessB
+:startContinueNextProcess
+echo characters : !count! over the char Limit : %charLimit% 
+set /p proceed=Warning: Header character count is %charLimit% or more. Continue? (Y/N):
+if /i "!proceed!"=="N" (
+    echo Exiting the program.
+    exit /b
+)
+goto endContinueNextProcess
+:nextprocessB
+
+:: START GREP PROCESS
+echo:  
+echo START GREPS
+
 :: Write header to CSV
 echo !header! > csvtest.csv
 
@@ -22,15 +78,13 @@ for /f "tokens=*" %%f in (file_list.txt) do (
     @echo === %%f ==============================
     :: Search for each Grep target
     for /f %%a in (grep_list.txt) do (
-        @echo === %%f === %%a
         findstr /m /c:"%%a" "%%f" > nul
         if errorlevel 1 (
-            set "result=!result!,FALSE"
+            set "result=!result!,"
         ) else (
-            set "result=!result!,TRUE"
+            set "result=!result!,1"
         )
     )
-    
     echo !result! >> csvtest.csv
 )
 
@@ -40,19 +94,19 @@ for /f "tokens=*" %%f in (file_list.txt) do (
 :: FOR DEBUG  set the end time
 set "end_time=%time%"
 
-:: FOR DEBUG  Calculate the elapsed time (in this example, calculated in seconds)
-:: FOR DEBUG  Show the individual components of the time
+:: FOR DEBUG  Calculate the elapsed time
 for /f "tokens=1-3 delims=:" %%a in ("%start_time%") do (
-    :: echo Debug: Hour=%%a, Minute=%%b, Second=%%c
     for /f "tokens=1 delims=." %%x in ("%%c") do set "clean_second=%%x"
     set /a "start=%%a*3600 + %%b*60 + clean_second"
 )
-:: FOR DEBUG  Calculate the elapsed time (in this example, calculated in seconds)
 for /f "tokens=1-3 delims=:" %%a in ("%end_time%") do (
     for /f "tokens=1 delims=." %%x in ("%%c") do set "clean_second=%%x"
     set /a "end=%%a*3600 + %%b*60 + clean_second"
 )
 
+:: FINISHED GREP PROCESS
+echo:  
+echo FINISHED GREPS
 :: FOR DEBUG  Display the start time
 @echo Start Time: %start_time%
 :: FOR DEBUG  Display the end time
